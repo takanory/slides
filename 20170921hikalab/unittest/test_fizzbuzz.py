7ccbf86cf9536933a56db56c40f14fdaf7cc0599
Takanori Suzuki <takanori@takanory.net>
2017-09-20 16:57:11 +0900

    update unittest code

--- 20170921hikalab/unittest/test_fizzbuzz.py
+++ 20170921hikalab/unittest/test_fizzbuzz.py
@@ -2,11 +2,11 @@ import unittest
 from fizzbuzz_ng import fizzbuzz
 
 class TestFizzbuzz(unittest.TestCase):
-    def test_num(self):
+    def test_num(self):  # 普通の数字を返す
         self.assertEqual(fizzbuzz(7), '7')
-    def test_fizz(self):
+    def test_fizz(self):  # 3の倍数
         self.assertEqual(fizzbuzz(99), 'Fizz')
-    def test_buzz(self):
+    def test_buzz(self):  # 5の倍数
         self.assertEqual(fizzbuzz(25), 'Buzz')
-    def test_fizzbuzz(self):
+    def test_fizzbuzz(self):  # 15の倍数
         self.assertEqual(fizzbuzz(30), 'FizzBuzz')
