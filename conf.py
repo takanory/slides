645ea251b559639fea3bcc8a71319749c00fa2e5
Takanori Suzuki <takanori@takanory.net>
2022-06-13 19:38:16 +0900

    refs #53 use furo theme

--- conf.py
+++ conf.py
@@ -47,7 +47,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'env', 'README.rst',
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'karma_sphinx_theme' # 'press'
+html_theme = 'furo'
 
 html_theme_options = {
     'nosidebar': False,
