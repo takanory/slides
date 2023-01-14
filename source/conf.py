51b3233fb62b20d550c72a54cc656a84ac597f85
Takanori Suzuki <takanori@takanory.net>
2023-01-14 12:50:20 +0900

    update font-awesome

--- source/conf.py
+++ source/conf.py
@@ -58,7 +58,7 @@ html_logo = "_static/takanory.jpg"
 html_title = "takanory slides"
 
 html_css_files = [
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css",
 ]
 
 html_sidebars = {
