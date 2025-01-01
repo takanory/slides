a1f5d13548ea7db5bb4c2fff1fa0eb7b713bb2b8
Takanori Suzuki <takanori@takanory.net>
2025-01-01 16:09:00 +0900

    refs #125 update font awesome and hightligh.js

--- source/conf.py
+++ source/conf.py
@@ -59,7 +59,7 @@ html_logo = "_static/takanory.jpg"
 html_title = "takanory slides"
 
 html_css_files = [
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css",
 ]
 
 html_sidebars = {
